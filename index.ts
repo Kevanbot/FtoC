ce471f766ab1b0cc271b4f14e5a27d71d0351a6a
Kevan Bergam <k_bergam@yahoo.com>
2021-03-19 16:46:19 -0600

    made changes

--- index.ts
+++ index.ts
@@ -1,25 +1,25 @@
 const tinput = require('prompt-sync')({sigint: true});
-let tempF = tinput('what is the temp in F?');
+let tempF = tinput('What is the temperature in Fahrenheit?');
 let tempFnum = Number(tempF);
 if (typeof tempFnum === 'number' && isNaN(tempFnum) == false) {
     let tempC:number = (tempFnum - 32)*5/9;
     if (tempC >40){
-        console.log(`It's ${tempC} degrees Celsius out. Wow thats Hot! Damn Hot! Oof! das a hot one!`)
+        console.log(`It's ${tempC} degrees Celsius out.`)
     }
     else {
-        console.log(`It's ${tempC} degrees Celsius out. Wow thats cold! Your nips must be razorsharp!`)
+        console.log(`It's ${tempC} degrees Celsius out.`)
     }
 }
 else {
     while (isNaN(tempFnum)) {
-    var tempFerror = tinput(`what is the temp in Fahrenheit!?! "${tempF}" is not a number you ignoramous!`);
+    var tempFerror = tinput(`What is the temperature in Fahrenheit!?! "${tempF}" is not a number`);
     tempFnum = Number(tempFerror);}
     let tempCerror:number = (tempFnum - 32)*5/9;
-        if (tempCerror >32){
-        console.log(`It's ${tempCerror} degrees Celsius out. Wow thats Hot!`)
+        if (tempCerror >28){
+        console.log(`It's ${tempCerror} degrees Celsius out.`)
         }
         else {
-        console.log(`It's ${tempCerror} degrees Celsius out. Wow thats cold!`)
+        console.log(`It's ${tempCerror} degrees Celsius out.`)
         }
     }
 
