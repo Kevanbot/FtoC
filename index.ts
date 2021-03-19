7f359e9594d65c8cc53263a201128d2d5c69dfa3
Kevan Bergam <k_bergam@yahoo.com>
2021-03-18 20:10:48 -0600

    finished function

--- index.ts
+++ index.ts
@@ -1,16 +1,29 @@
-let message: string = 'Hello World! What is the temperature today in Fahrenheit?';
-console.log(message);
-
-const prompt:number = require('prompt-sync')({sigint: true});
-
-
-
-function FtoC (tempF: number){
-    let tempC:number = (tempF - 32)*5/9
+const tinput = require('prompt-sync')({sigint: true});
+let tempF = tinput('what is the temp in F?');
+let tempFnum = Number(tempF);
+if (typeof tempFnum === 'number' && isNaN(tempFnum) == false) {
+    let tempC:number = (tempFnum - 32)*5/9;
     if (tempC >40){
-        return console.log("It's ${tempC} degrees Celsius out. Wow thats Hot!")
+        console.log(`It's ${tempC} degrees Celsius out. Wow thats Hot!`)
     }
-    if (tempC <10){
-        return console.log("It's ${tempC} degrees Celsius out. Wow thats Cold!")
+    else {
+        console.log(`It's ${tempC} degrees Celsius out. Wow thats cold!`)
     }
 }
+else {
+    while (isNaN(tempFnum)) {
+    const tempFerror = tinput('what is the temp in F? Thats not a number you ignoramous!');
+    tempFnum = Number(tempFerror);}
+    let tempCerror:number = (tempFnum - 32)*5/9;
+        if (tempCerror >40){
+        console.log(`It's ${tempCerror} degrees Celsius out. Wow thats Hot!`)
+        }
+        else {
+        console.log(`It's ${tempCerror} degrees Celsius out. Wow thats cold!`)
+        }
+    }
+
+
+
+
+
